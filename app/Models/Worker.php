8cfc297de33abf65e8906535e1126284ffd4d93f
ShevaMax26 <shevchuk.maxym@gmail.com>
2023-11-17 16:30:47 +0200

    relationship: many to many

--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Worker extends Model
@@ -29,5 +30,10 @@ class Worker extends Model
     public function position(): BelongsTo
     {
         return $this->belongsTo(Position::class);
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
     }
 }
---
+++ database/migrations/2023_11_17_133006_create_project_worker_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_worker', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Project::class)
+                ->index()
+                ->unique()
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->foreignIdFor(\App\Models\Worker::class)
+                ->index()
+                ->unique()
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_workers');
+    }
+};
