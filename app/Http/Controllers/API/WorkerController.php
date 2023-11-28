009ce3d74cde43edb57277cb496ae5e24868ffd9
ShevaMax26 <shevchuk.maxym@gmail.com>
2023-11-28 17:48:03 +0200

    rest api

---
+++ app/Http/Controllers/API/WorkerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\WorkerResource;
+use App\Models\Worker;
+use Illuminate\Http\Request;
+
+class WorkerController extends Controller
+{
+    public function index()
+    {
+        $workers = Worker::all();
+
+        return WorkerResource::collection($workers)->resolve();
+    }
+
+    public function show(Worker $worker)
+    {
+        return WorkerResource::make($worker)->resolve();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('workers', [\App\Http\Controllers\API\WorkerController::class, 'index']);
+Route::get('workers/{worker}', [\App\Http\Controllers\API\WorkerController::class, 'show']);
