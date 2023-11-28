389afee566c5fd078b8f1a0d6692379df45788c3
ShevaMax26 <shevchuk.maxym@gmail.com>
2023-11-28 18:43:39 +0200

    rest api for workers

--- app/Http/Controllers/API/WorkerController.php
+++ app/Http/Controllers/API/WorkerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Worker\StoreRequest;
+use App\Http\Requests\Worker\UpdateRequest;
 use App\Http\Resources\WorkerResource;
 use App\Models\Worker;
 use Illuminate\Http\Request;
@@ -19,5 +21,28 @@ class WorkerController extends Controller
     public function show(Worker $worker)
     {
         return WorkerResource::make($worker)->resolve();
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        $worker = Worker::create($data);
+        return WorkerResource::make($worker)->resolve();
+    }
+
+    public function update(UpdateRequest $request, Worker $worker)
+    {
+        $data = $request->validated();
+        $worker->update($data);
+        $worker->fresh();
+        return WorkerResource::make($worker)->resolve();
+    }
+
+    public function destroy(Worker $worker)
+    {
+        $worker->delete();
+        return response()->json([
+            'message' => 'worker was deleted'
+        ]);
     }
 }
